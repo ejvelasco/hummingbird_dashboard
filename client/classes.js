cce4f6fe259d6e93943f027a52656a15aafda2ba
velascoDev <dracontium2@gmail.com>
2017-03-14 17:33:44 -0500

    Added delete new post button

--- client/classes.js
+++ client/classes.js
@@ -8,7 +8,7 @@ Template.classes.events({
 	'click #add': function(){
 		$('#add').animate({height: '300px'}, 200, function(){
 			$('#add').text('');
-			$('#add').append('<div class="caption"><input type="text" placeholder="Class Title"></input></br><textarea placeholder="Class Description"></textarea></br><button id="create" type="button" class="btn btn-primary" onclick="this.blur();"><span class="glyphicon glyphicon-plus"></span> &nbsp;Create</button></div>');
+			$('#add').append('<div class="caption"><a href="#"><span class="glyphicon glyphicon-remove"></span></a><input type="text" placeholder="Class Title"></input></br><textarea placeholder="Class Description"></textarea></br><button id="create" type="button" class="btn btn-primary" onclick="this.blur();"><span class="glyphicon glyphicon-plus"></span> &nbsp;Create</button></div>');
 			setTimeout(function(){
 				$('#add').removeClass('unclicked');
 				$('#add').addClass('clicked');

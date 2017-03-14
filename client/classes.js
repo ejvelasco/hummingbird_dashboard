c66bd77ab3f743d2979dff6c32988ea57f654258
velascoDev <dracontium2@gmail.com>
2017-03-14 18:00:31 -0500

    Users can now create posts

--- client/classes.js
+++ client/classes.js
@@ -7,15 +7,21 @@ Template.classes.helpers({
 Template.classes.events({
 	'click #add': function(){
 		$('#add').animate({height: '300px'}, 200, function(){
-			$('#add').text('');
-			$('#add').append('<div class="caption"><a href="#"><span class="glyphicon glyphicon-remove"></span></a><input type="text" placeholder="Class Title"></input></br><textarea placeholder="Class Description"></textarea></br><button id="create" type="button" class="btn btn-primary" onclick="this.blur();"><span class="glyphicon glyphicon-plus"></span> &nbsp;Create</button></div>');
+			setTimeout(function(){
+				$('#add').text('');
+			$('#add').append('<div class="caption"><a href="#"><span class="glyphicon glyphicon-remove"></span></a><input id="class-title" type="text" placeholder="Class Title"></input></br><textarea id="class-desc" placeholder="Class Description"></textarea></br><button id="create" type="button" class="btn btn-primary" onclick="this.blur();"><span class="glyphicon glyphicon-plus"></span> &nbsp;Create</button></div>');
+			}, 200);
 			setTimeout(function(){
 				$('#add').removeClass('unclicked');
 				$('#add').addClass('clicked');
 				$('#add').removeAttr('id');
 			},400);
 		});
-		
+	},
+	'click #create': function(){
+		var title = $('#class-title').val();
+		var desc =  $('#class-desc').val();
+		Classes.insert({title: title, desc: desc, date:  new Date() });	
 	}
 })
  

fed46e32d8f5cea9c4ecea147f479c31640993e5
velascoDev <dracontium2@gmail.com>
2017-03-17 02:26:38 -0500

    Fixed issues caused by lecture count

--- client/helpers/classes.js
+++ client/helpers/classes.js
@@ -1,4 +1,4 @@
-var classTitle, classDesc, parentId, grandParentObj, parentClass,id, date, classInfo, originalHeight;
+var classTitle, classDesc, parentId, grandParentObj, parentClass,id, date, classInfo, originalHeight, lectureCount;
 
 Template.classes.onCreated(function classesOnCreated() {
   Meteor.subscribe('classes');
@@ -32,19 +32,23 @@ Template.classes.events({
 		parentId = '#'+(event.currentTarget.parentNode).parentNode.getAttribute('id');
 		parentClass = '.'+event.currentTarget.parentNode.getAttribute('class');
 		classTitle = $(parentId+' .title').text();
+		lectureCount = classTitle.lastIndexOf(' ');
+		lectureCount = classTitle.substring(lectureCount, classTitle.length);
+		classTitle = classTitle.substring(0, classTitle.indexOf('LECTURES'));
+		classTitle = classTitle.replace(/\s+$/, '');
 		classDesc = $(parentId+' .desc').text();
 		date = $(parentId+' .date').text();
 		originalHeight = $(parentId).height();
 		console.log(originalHeight);
 		$(parentId).animate({height: '300px'}, 200 , function(){
 			$(parentId).text('');
-			$(parentId).append('<div class="caption"><a href="#" class="discard-edit"><span class="glyphicon glyphicon-remove"></span></a><input class="class-title" type="text" placeholder="Class Title" value="'+classTitle+'"></input></br><textarea placeholder="Class Description" class="class-desc">'+classDesc+'</textarea></br><button type="button" class="btn btn-primary update" onclick="this.blur();"><span class="glyphicon glyphicon-ok"></span> &nbsp;Update</button></div>');	
+			$(parentId).append('<div class="caption"><a href="#" class="discard-edit"><span class="glyphicon glyphicon-remove control"></span></a><input class="class-title" type="text" placeholder="Class Title" value="'+classTitle+'"></input></br><textarea placeholder="Class Description" class="class-desc">'+classDesc+'</textarea></br><button type="button" class="btn btn-primary update" onclick="this.blur();"><span class="glyphicon glyphicon-ok"></span> &nbsp;Update</button></div>');	
 		});
 	},
 	'click .discard-edit': function(){
 		$(parentId).animate({height: originalHeight+10}, 200 , function(){
 			$(parentId).text('');
-			$(parentId).append('<div class="caption"><a href="#" class="delete"><span class="glyphicon glyphicon-remove"></span></a><a href="#" class="edit"><span class="glyphicon glyphicon-pencil"></span></a><p class="title">'+classTitle+'</p><p class="desc">'+classDesc+'</p><p class= "date">'+date+'</p> </div>');	
+			$(parentId).append('<div class="caption"><a href="#" class="delete control"><span class="glyphicon glyphicon-remove"></span></a><a href="#" class="edit control"><span class="glyphicon glyphicon-pencil"></span></a><p class="title">'+classTitle+'<a class="lecture-count"><span class="glyphicon glyphicon-book small-icon"></span> LECTURES - '+lectureCount+'</a></p><p class="desc">'+classDesc+'</p><p class= "date">'+date+'</p> </div>');	
 		});
 	},
 	'click .delete': function(event){
@@ -53,7 +57,8 @@ Template.classes.events({
 		$(grandParentObj).animate({opacity: 0, height: 0}, 400, function(){
 			id = parentId.replace(/#/g, '');
 			Meteor.call('Classes.remove', id);
-		});	
+		});
+
 	},
 	'click .update': function(event){
 		parentId = '#'+(event.currentTarget.parentNode).parentNode.getAttribute('id');
@@ -62,9 +67,10 @@ Template.classes.events({
 		classDesc = $(parentId+' .class-desc').val();
 		$(parentId).animate({height: originalHeight+10}, 200 , function(){
 			$(parentId).text('');
-			$(parentId).append('<div class="caption"><a href="#" class="delete"><span class="glyphicon glyphicon-remove"></span></a><a href="#" class="edit"><span class="glyphicon glyphicon-pencil"></span></a><p class="title">'+classTitle+'</p><p class="desc">'+classDesc+'</p><p class= "date">'+'Updated on '+new Date()+'</p> </div>');	
+			$(parentId).append('<div class="caption"><a href="#" class="delete control"><span class="glyphicon glyphicon-remove"></span></a><a href="#" class="edit control"><span class="glyphicon glyphicon-pencil"></span></a><p class="title">'+classTitle+'<a class="lecture-count"><span class="glyphicon glyphicon-book small-icon"></span> LECTURES - '+lectureCount+'</a></p><p class="desc">'+classDesc+'</p><p class= "date">'+'Updated on '+new Date()+'</p> </div>');	
 		});
 		id = parentId.replace(/#/g, '');
+
 		classInfo = {
 			id: id, 
 			info: {title: classTitle, desc: classDesc}

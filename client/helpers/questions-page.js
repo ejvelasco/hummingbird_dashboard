90521c898ab7372ce7cd20d2d260e63891c8b959
velascoDev <dracontium2@gmail.com>
2017-03-19 17:41:44 -0500

    Improved questions controlse

--- client/helpers/questions-page.js
+++ client/helpers/questions-page.js
@@ -1,9 +1,10 @@
-var questionText, questionId, questionDate, questions, currentDate, currentDate, datePosted, index;
+var questionText, questionId, questionDate, questions, currentDate, currentDate, index, row;
 function formatTime(duration) {
     duration = duration/1000;
     var days = 0;
     var hours = 0;
     var minutes = 0;
+    var datePosted = 0;
     while(duration >= 3600*24){
       days++;
       duration = duration - 24*3600;
@@ -16,7 +17,21 @@ function formatTime(duration) {
       minutes++
       duration = duration-60;
     }
-    return {days: days, hours: hours, minutes: minutes};
+    datePosted = {days: days, hours: hours, minutes: minutes};
+    if(datePosted.days === 1){
+    	datePosted = "1 day"; 
+    } else if(datePosted.days > 1){
+    	datePosted = datePosted.days+" days" ;
+    } else if(datePosted.hours === 1){
+    	datePosted = "1 hr";
+    } else if(datePosted.hours > 1){
+    	datePosted = datePosted.hours+" hrs";
+    }else if(datePosted.minutes >=0 && datePosted.minutes <= 1){
+    	datePosted = "1 min";
+    } else if(datePosted.minutes > 1){
+    	datePosted = datePosted.minutes+" min";
+    }
+    return datePosted;
 }
 Template.questionsPage.onCreated(function questionsOnCreated() {
   Meteor.subscribe('questions');
@@ -30,20 +45,7 @@ Template.questionsPage.helpers({
 	questions : function(){
 	currentDate = new Date();
 	questions = Questions.find({}, {sort:{date: -1}}).map(function(question){
-		datePosted = formatTime(currentDate.getTime() - question.date);
-		if(datePosted.days === 1){
-			question.datePosted = "1 day"; 
-		} else if(datePosted.days > 1){
-			question.datePosted = datePosted.days+" days" ;
-		} else if(datePosted.hours === 1){
-			question.datePosted = "1 hr";
-		} else if(datePosted.hours > 1){
-			question.datePosted = datePosted.hours+" hrs";
-		}else if(datePosted.minutes >=0){
-			question.datePosted = "1 min";
-		} else if(datePosted.minutes > 1){
-			question.datePosted = datePosted.minutes+" min";
-		}
+		question.datePosted = formatTime(currentDate.getTime() - question.date);
 		return question;
 	});
 	return questions;
@@ -52,7 +54,9 @@ Template.questionsPage.helpers({
 Template.questionsPage.events({
 	'click #new-question-submit': function(event){
 		questionText = $('#question-input').val();
-		Meteor.call('Questions.insert', {text: questionText});
+		Meteor.call('Questions.insert', {text: questionText});			
+		$('#question-input').val('');
+		$('.collapse').collapse('hide');	
 	},
 	'click #discard-new-question': function(event){
 		$('.collapse').collapse('hide');
@@ -61,6 +65,26 @@ Template.questionsPage.events({
 	'click .question-menu-dd': function(event){
 		questionId = $(event.currentTarget.parentNode.parentNode.parentNode).attr('id');
 		$('#'+questionId+ ' .question-edit').animate({height: 'toggle'}, 200);
-	} 
+	}, 
+	'click .question-item-delete': function(event){
+		questionId = $(event.currentTarget.parentNode.parentNode.parentNode.parentNode).attr('id');
+		row = $(event.currentTarget.parentNode.parentNode.parentNode.parentNode.parentNode).animate({opacity: 0, height: 'toggle'}, 300);
+		setTimeout(function(){
+			Meteor.call('Questions.remove', questionId);	
+		}, 1000);
+	},
+	'click .question-item-edit': function(event){
+		questionId = $(event.currentTarget.parentNode.parentNode.parentNode.parentNode).attr('id');
+		// row = $(event.currentTarget.parentNode.parentNode.parentNode.parentNode.parentNode).animate({opacity: 0, height: 'toggle'}, 300);
+		questionText = $('#'+questionId+ '.thumbnail .question').text();
+		$('#question-edit-input').val(questionText);
+		$('#'+questionId+ ' .question-edit').animate({height: 'toggle'}, 200);
+		$('#question-edit').fadeIn(300);
+		$('.mask').fadeIn(300);
+	},
+	'click .question-edit-discard': function(event){
+		$('#question-edit').fadeOut(300);
+		$('.mask').fadeOut(300);
+	}
 
 });

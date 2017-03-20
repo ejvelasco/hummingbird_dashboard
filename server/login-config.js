bcf404fa6001b279820ed9e1025a05cdaa0895f3
Eduardo Velasco <dracontium2@gmail.com>
2017-03-19 23:33:55 -0500

    Delete login-config.js

--- server/login-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-    service: 'facebook'
-});
- 
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '171694710004750',
-    secret: '17b5a5f7bd3ab9b379474cfe1a7519f5'
-});

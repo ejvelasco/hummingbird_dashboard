09db2fe37a6f6c60dc5c67e60d34a693feefe49d
velascoDev <dracontium2@gmail.com>
2017-03-29 01:54:39 -0500

    Bug fixes

--- client/helpers/questions-page.js
+++ client/helpers/questions-page.js
@@ -1,14 +1,16 @@
+//declare variables
 var questionText, questionId, questionDate, questions, currentDate, currentDate, index, row, identifier, studentId, ownQuestion, starClass, idx, alreadyStarred, comments, commentText, lastQuestionId;
+var starredByUser = [], newQuestionId, re;
+//npm packages
 var uuidV4 = require('uuid/v4');
-var starredByUser = [];
-var newQuestionId;
-var re;
+//keep track of starred by user
 starredByUser = document.cookie;
 idx = starredByUser.indexOf('=');
 starredByUser = starredByUser.substring(idx+1, starredByUser.length).split(',');
 if(starredByUser[0] === ''){
 	starredByUser = [];
 }
+//format question and comment time
 function formatTime(duration) {
     duration = duration/1000;
     var days = 0;
@@ -43,13 +45,18 @@ function formatTime(duration) {
     }
     return datePosted;
 }
+//on created
 Template.questionsPage.onCreated(function questionsPageOnCreated() {
-  document.title = "Questions - Live";
-  Session.set("questionsPage", true);
-  if(localStorage.studentId === undefined){
-	localStorage.studentId = uuidV4();
-  }
+	//set title
+  	document.title = "Questions - Live";
+  	//show new question button
+  	Session.set("questionsPage", true);
+  	//create id to identify student
+  	if(localStorage.studentId === undefined){
+		localStorage.studentId = uuidV4();
+  	}
 });
+//on rendered
 Template.questionsPage.onRendered(function questionsPageOnRendered(){
 	$('#questions-container').fadeIn(400);
 });
@@ -57,6 +64,7 @@ Template.questionsPage.helpers({
 	questions : function(){
 	currentDate = new Date();
 	questions = Questions.find({parentLecture: Session.get("lectureId")}, {sort:{date: -1}}).map(function(question){
+		//determine if the question is owned, format date posted, comment date poster, stars, etc.
 		question.isOwned = (localStorage.studentId === question.owner);
 		question.datePosted = formatTime(currentDate.getTime() - question.date);
 		question.updatedOn = formatTime(currentDate.getTime() - question.updated);
@@ -74,15 +82,21 @@ Template.questionsPage.helpers({
 	return questions;
 	}
 });
+//events
 Template.questionsPage.events({
 	'click #new-question-submit': function(event){
+		// get new question text
 		questionText = $('#question-input').val();
+		// insert question 
 		Meteor.call('Questions.insert', {text: questionText, owner: localStorage.studentId, parentLecture: Session.get("lectureId"), stars: 0, comments: []});
+		// update question count
 		Meteor.call('Lectures.updateQuestionCount', {lectureId: Session.get("lectureId")});			
+		// clear and collapse new question input
 		$('#question-input').val('');
 		$('.collapse').collapse('hide');	
 	},
 	'keypress #new-question-ask input': function (event) {
+	    //allow question submission by enter key
 	    if(event.which === 13){
 	    	questionText = $('#question-input').val();
 	    	Meteor.call('Questions.insert', {text: questionText, owner: localStorage.studentId, parentLecture: Session.get("lectureId"), stars: 0, comments: []});
@@ -92,43 +106,49 @@ Template.questionsPage.events({
 	    }
 	},
 	'click #discard-new-question': function(event){
+		//collapse and clear new question div
 		$('.collapse').collapse('hide');
 		$('#question-input').val('');
-	},
-	'click .question-menu-dd': function(event){
-		questionId = $(event.currentTarget.parentNode.parentNode.parentNode).attr('id');
-		$('#'+questionId+ ' .question-edit').animate({height: 'toggle'}, 200);
 	}, 
 	'click .question-item-delete': function(event){
+		//get id of question to delete
 		questionId = $(event.currentTarget.parentNode.parentNode.parentNode).attr('id');
 		row = $(event.currentTarget.parentNode.parentNode.parentNode.parentNode).animate({opacity: 0, height: 'toggle'}, 300);
+		//remove question and update method count
 		setTimeout(function(){
 			Meteor.call('Questions.remove', questionId);
 			Meteor.call('Lectures.updateQuestionCount', {lectureId: Session.get("lectureId")});	
 		}, 300);
 	},
 	'click .question-item-edit': function(event){
+		// get id of question to edit 
 		questionId = $(event.currentTarget.parentNode.parentNode.parentNode).attr('id');
 		questionText = $('#'+questionId+ '.thumbnail .question').text();
+		//set question info in edit input
 		$('#question-edit-input').val(questionText);
-		$('#'+questionId+ ' .question-edit').animate({height: 'toggle'}, 200);
+		//fade in edit and mask divs
 		$('#question-edit').fadeIn(300);
 		$('.mask').fadeIn(300);
 	},
 	'click .question-edit-discard': function(event){
+		//fade out question and mask divs
 		$('#question-edit').fadeOut(300);
 		$('.mask').fadeOut(300);
 	}, 
 	'click #question-edit-submit': function(event){
+		//get text of question to update
 		questionText = $('#question-edit-input').val();
+		//update method
 		Meteor.call('Questions.update', {id: questionId, text: questionText});
 		$('#question-edit').fadeOut(300);
+		//fade out mask and edit divs, clear edit input
 		$('.mask').fadeOut(300);
 		setTimeout(function(){
 			$('#question-edit-input').val('');
 		}, 500);
 	},
 	'click .mask': function(event){
+		// fade out mask and edit, and wolfram response divs, clear edit input
 		$('#question-edit').fadeOut(300);
 		$('#wolfram-response').fadeOut(300);
 		$('.mask').fadeOut(300);
@@ -138,10 +158,13 @@ Template.questionsPage.events({
 		}, 500);
 	}, 
 	'click .like': function(event){
+		///get info of question starred
 		starClass = $(event.currentTarget).attr('class');
 		questionId = $(event.currentTarget.parentNode.parentNode.parentNode).attr('id');
+		//handle case where question is already starred
 		alreadyStarred = starredByUser.find(function(id) { return id === questionId; });
 		if(alreadyStarred){
+			//remove star
 			$(event.currentTarget).removeClass('glyphicon-star');
 			$(event.currentTarget).addClass('glyphicon-star-empty');
 			idx = starredByUser.indexOf(questionId);
@@ -149,6 +172,7 @@ Template.questionsPage.events({
 			document.cookie = 'starredByUser='+starredByUser;
 			Meteor.call('Questions.updateStarCount', {id: questionId, amount: -1});
 		} else{
+			// add star
 			$(event.currentTarget).addClass('glyphicon-star');
 			$(event.currentTarget).removeClass('glyphicon-star-empty');
 			starredByUser.push(questionId);
@@ -157,10 +181,12 @@ Template.questionsPage.events({
 		}
 	},
 	'click .comment-toggle': function(event){
+		//show or hide comments
 		questionId = $(event.currentTarget.parentNode.parentNode.parentNode).attr('id');
 		$('#'+questionId+' .comments').animate({height: 'toggle', opacity: 'toggle'}, 300);
 	},
 	'keypress .comment input': function (event) {
+		// submit comments with enter, insert comment
 		if(event.which === 13){
 			commentText = $(event.currentTarget).val();
 			Meteor.call('Questions.insertComment', {id: questionId, commentText: commentText, });
@@ -168,36 +194,44 @@ Template.questionsPage.events({
 		}
 	},
 	'click .comment input': function(event){
+		// adjust questionId to clicked question
 		questionId = $(event.currentTarget.parentNode.parentNode.parentNode).attr('id');
 	},
 	'click .comment-delete': function(event){
+		//get comment info and delete comment method
 		newQuestionId = $(event.currentTarget.parentNode.parentNode.parentNode).attr('id');
 		commentId = $(event.currentTarget.parentNode).attr('id');
 		Meteor.call('Questions.deleteComment', {id: newQuestionId, commentId: commentId});
 	}, 
 	'click #wolfram-logo':function(event){
-		re  = new RegExp('/', 'g');
+		//encode wolfram query
 		questionText = encodeURIComponent($('#question-input').val());
+		//show loading
 		$('#questions-page-spinner').fadeIn('fast');
+		//wolfram request
 		$.get( "/queryWolfram/"+questionText, function(data) {
 		  var responseObjs = JSON.parse(data);
 		  $('#responses-container').html('');
 		  for(obj in responseObjs){
-		  		$('#responses-container').append('<div class="response-row"><p>'+responseObjs[obj].title+':</p><img src="'+responseObjs[obj].subpods[0].image+'"></div>')
-		  		// console.log(responseObjs[obj].subpods[0].image);
+		  		//select title and images and append to wolfram responses div
+		  		$('#responses-container').append('<div class="response-row"><p>'+responseObjs[obj].title+':</p><img src="'+responseObjs[obj].subpods[0].image+'"></div>');
 		  }
+		  //show mask and response div
 		  $('.mask').fadeIn(500);
 		  $('#wolfram-response').fadeIn(500);
+		  //collapse new question div
 		  $('.collapse').collapse('hide');
+		  //hide spinner
 		  $('#questions-page-spinner').fadeOut('fast');
 		});
 	},
 	'click #wolfram-response-discard': function(event){
+		//hide wolfram response div and clear for next use
 		$('#wolfram-response').fadeOut(300);
 		$('.mask').fadeOut(300);
 		setTimeout(function () {
 			$('#responses-container').html('');
 			$('#question-input').val('');
-		}, 300)
+		}, 300);
 	}
 });
--- client/helpers/resources.js
+++ client/helpers/resources.js
@@ -1,31 +1,36 @@
+//rendered
 Template.resources.onRendered(function resourcesOnCreated() {
-  loadingTemplate: 'loading',
-  document.title = "Lecture Resources";
-  Session.set('questionsPage', false);
-  if(localStorage.studentId === undefined){
-	localStorage.studentId = uuidV4();
-  }
-  var YouTube = require('youtube-node');
-
-  var youTube = new YouTube();
-
-  youTube.setKey('AIzaSyBAnTal247NdVZx27NILoAvohopbLugPXs');
-  youTube.addParam('order', 'viewCount');
-  youTube.search(Session.get('lectureTitle'), 6, function(error, result) {
-    if (error) {
-      console.log(error);
+    //show spinner
+    loadingTemplate: 'loading',
+    //set title
+    document.title = "Lecture Resources";
+    Session.set('questionsPage', false);
+    //identify student if needed
+    if(localStorage.studentId === undefined){
+	   localStorage.studentId = uuidV4();
     }
-    else {
-      var videos = result.items;
-      for(video in videos){
-      	$('#resources-container').append('<iframe width="300" class="resource-video" height="250" src="https://www.youtube.com/embed/'+videos[video].id.videoId+'" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>');
-      }
-    }
-
-    setTimeout(function(){
-    	$('.spinner-container').fadeOut('slow');
-    	$('#resources-container').fadeIn('slow');
-    }, 1500)
+    //youtube package
+    var YouTube = require('youtube-node');
+    var youTube = new YouTube();
+    //set key and add parameters
+    youTube.setKey('AIzaSyBAnTal247NdVZx27NILoAvohopbLugPXs');
+    youTube.addParam('order', 'viewCount');
+    // youtube request
+    youTube.search(Session.get('lectureTitle'), 6, function(error, result) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            var videos = result.items;
+            for(video in videos){
+                // append videos to video container
+      	        $('#resources-container').append('<iframe width="300" class="resource-video" height="250" src="https://www.youtube.com/embed/'+videos[video].id.videoId+'" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe>');
+            }
+        }
+        //hide spinner and show videos
+        setTimeout(function(){
+    	   $('.spinner-container').fadeOut('slow');
+    	   $('#resources-container').fadeIn('slow');
+        }, 1500)
   });
-
 });
